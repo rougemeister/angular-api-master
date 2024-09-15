ed25aa97584c515e19180d383a5f28c58c170b45
rougemeister <rougemeister1@gmail.com>
2024-09-15 19:08:09 +0000

    cofigured routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostCreateComponent } from './components/post-create/post-create.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/create', component: PostCreateComponent },
+  { path: 'posts/:id/edit', component: PostEditComponent },
+  {
+    path: 'posts/:id',
+    loadComponent: () => import('./components/post-detail/post-detail.component')
+      .then(m => m.PostDetailComponent)
+  }
+];
